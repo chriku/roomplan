3921039708c544f75640cd5c05df70e0c98688e9
Christian Kurz <chrikuvellberg@gmail.com>
2026-01-30 03:33:06 +0100

    date range overlap

--- src/model/date_range.ts
+++ src/model/date_range.ts
@@ -1,5 +1,7 @@
 export class DateRange {
-    constructor(readonly startTime: Date, readonly endTime: Date) { }
+    constructor(readonly startTime: Date, readonly endTime: Date) {
+        if (!(endTime > startTime)) throw Error("Not a valid DateRange");
+    }
     contains(date: Date): boolean { return (date >= this.startTime) && (date < this.endTime); }
-    overlaps(other: DateRange): boolean { throw Error("NotYetImplemented"); }
+    overlaps(other: DateRange): boolean { return (this.startTime < other.endTime) && (other.startTime < this.endTime); }
 }
