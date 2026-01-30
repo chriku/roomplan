29a1ecbdcd6889d1c0db762c83dc7aaf13793bd3
Christian Kurz <chrikuvellberg@gmail.com>
2026-01-30 03:38:57 +0100

    slim down room

--- src/model/room.ts
+++ src/model/room.ts
@@ -1,8 +1,14 @@
-import { v4 as uuidv4 } from 'uuid';
 import type { Booking } from './booking.js';
+
+export abstract class RoomProvider {
+    static singleton: RoomProvider | null = null;
+
+    abstract listRooms(): Room[];
+    abstract findRoom(name: string): Room | null;
+}
 
 export class Room {
     readonly bookings: Booking[] = [];
-    constructor(readonly name: string, readonly capacity: number, readonly id: string = uuidv4()) { }
+    constructor(readonly name: string, readonly capacity: number) { }
     isBooked(date: Date = new Date()) { return this.bookings.filter((it) => it.affects(date)).length > 0; }
 }
